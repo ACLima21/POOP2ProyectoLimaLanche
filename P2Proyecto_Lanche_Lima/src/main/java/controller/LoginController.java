ab086a20ddcba1bade972e13873a29f122a0e283
Sebastian25L <sebastianlanche9@gmail.com>
2025-01-19 22:31:13 -0500

    Login Validacion

--- P2Proyecto_Lanche_Lima/src/main/java/controller/LoginController.java
+++ P2Proyecto_Lanche_Lima/src/main/java/controller/LoginController.java
@@ -2,15 +2,22 @@ package controller;
 
 import view.LoginInterfaz;
 import view.RegisterInterfaz;
+import javax.swing.JLabel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class LoginController implements ActionListener {
 
     protected LoginInterfaz loginInt;
- 
+    private JLabel lbErrorUsername;
+    private JLabel lbErrorPassword;
+
     public LoginController(LoginInterfaz loginInt) {
         this.loginInt = loginInt;
+        this.lbErrorUsername = loginInt.lbErrorUsername;
+        this.lbErrorPassword = loginInt.lbErrorPassword;
+        this.loginInt.btnSignIn.addActionListener(this);
         this.loginInt.btnSignUp.addActionListener(this);
     }
 
@@ -20,10 +27,36 @@ public class LoginController implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == loginInt.btnSignUp) {
+        if (e.getSource() == loginInt.btnSignIn) {
+            String username = loginInt.tfUsername.getText();
+            char[] passwordArray = loginInt.pfPassword.getPassword();
+            String password = new String(passwordArray);
+
+            boolean isValid = true;
+
+            if (username.isEmpty()) {
+                lbErrorUsername.setText("Campo obligatorio");
+                isValid = false;
+            } else {
+                lbErrorUsername.setText("");
+            }
+
+            if (passwordArray.length == 0) {
+                lbErrorPassword.setText("Campo obligatorio");
+                isValid = false;
+            } else {
+                lbErrorPassword.setText("");
+            }
+
+            if (isValid) {
+                System.out.println("Iniciando sesión...");
+            }
+
+            Arrays.fill(passwordArray, '\0');
+        } else if (e.getSource() == loginInt.btnSignUp) {
             RegisterInterfaz registerInt = new RegisterInterfaz();
             registerInt.setVisible(true);
-            loginInt.dispose(); 
+            loginInt.dispose();
         }
     }
 }
